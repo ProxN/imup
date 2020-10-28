22574f4012aa876fe41abbe49dbee64b1c4c7180
proxn <idel.ayoubkan@gmail.com>
2020-10-28 16:17:35 +0100

    First commit

---
+++ client/src/styles/theme.styles.ts
@@ -0,0 +1,62 @@
+import { DefaultTheme } from 'styled-components';
+
+const fontSizes = [12, 14, 16, 18, 24, 34, 58, 72];
+
+const colors = {
+  primary: {
+    light: '#4f5461',
+    main: '#232939',
+    dark: '#1c212e',
+  },
+  secondary: {
+    light: '#9ed8dd',
+    main: '#86ced5',
+    dark: '#6ba5aa',
+  },
+  danger: {
+    light: '#ee8277',
+    main: '#e95e50',
+    dark: '#d04436',
+  },
+  success: {
+    light: '#6ddb9c',
+    main: '#43d17f',
+    dark: '#29b866',
+  },
+  info: {
+    light: '#7fe6fc',
+    main: '#5adffb',
+    dark: '#41c5e2',
+  },
+  warining: {
+    light: '#e0874d',
+    main: '#d7651a',
+    dark: '#be4c00',
+  },
+  text: {
+    main: '#1b1f26',
+    secondary: '#272c36',
+  },
+  textInverse: {
+    main: '#fff',
+    secondary: '#e9eaeb',
+  },
+  bg: '#1B202D',
+  bgInverse: '#fff',
+};
+
+const lineHeight = 1.5;
+const fontFamily = "'Poppins', Helvetica, sans-serif";
+const fontWeights = [400, 600, 700];
+
+const Theme: DefaultTheme = {
+  colors,
+  lineHeight,
+  fontFamily,
+  fontSizes,
+  fontWeights,
+  fontSizeBase: fontSizes[2],
+  borderRadius: '2px',
+};
+
+export default Theme;
